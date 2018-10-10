5864982c6560dee315454a74c5b097dff761c505
UENO Takahiro <uenotakahirooo@gmail.com>
2018-10-11 07:41:02 +0900

    Add list5-15

---
+++ Chap5/list5-15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+  int i,no;
+  unsigned long counter = 0;
+
+  for(no=2;no<=1000;no++){
+    for(i=2;i<no;i++){
+      counter++;
+      if(no%i==0)
+        break;
+    }
+    if(no==i)
+      printf("%d\n", no);
+  }
+
+  printf("乗除を行った回数:%lu\n", counter);
+
+  return(0);
+}
