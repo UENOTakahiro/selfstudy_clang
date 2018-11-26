2de149d68f108e92f1cc0a3498716afaf3e284e0
UENO Takahiro <uenotakahirooo@gmail.com>
2018-11-26 22:45:08 +0900

    Add. not completed.

---
+++ AOJ/ITP1_7_C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int r, c;
+  cin >> r >> c;
+
+  int vec[r+1][c+1];
+  for(int i=0;i<r+1;i++){
+    for(int j=0;j<c+1;j++){
+      vec[i][j] = 0;
+    }
+  }
+
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+      cin >> vec[i][j];
+    }
+  }
+
+  //行の集計
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+      vec[i][c] += vec[i][j];
+    }
+  }
+
+  //列の集計
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c+1;j++){
+      vec[c][j] += vec[i][j];
+    }
+  }
+
+  for(int i=0;i<r+1;i++){
+    for(int j=0;j<c+1;j++){
+      if(j==c){
+        cout << vec[i][j];
+      } else {
+        cout << vec[i][j] << " ";
+      }
+    }
+    cout << endl;
+  }
+}
