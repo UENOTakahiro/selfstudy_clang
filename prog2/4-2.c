e0448b318054e748bc734695b23464d586dc35fe
UENO Takahiro <uenotakahirooo@gmail.com>
2019-01-21 20:43:16 +0900

    add 4-2

---
+++ prog2/4-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long int f(int n);
+// n>=0 に対して
+// n番目のフィボナッチ数を計算して返す
+
+long int count = 0;
+
+int main(int argc, char *argv[]){
+  if( argc==1 ) return 0;
+  int n= atoi(argv[1]);
+  long int fib;
+  fib= f(n);
+  printf("%ld ", fib);
+  printf("[%ld times]\n",count);
+  return 0;
+}
+
+long int f(int n){
+  count++;
+  if( n<=1 ) return n;
+  return f(n-1)+f(n-2);
+}
