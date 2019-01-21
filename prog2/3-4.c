84b46c0f98bef7dc9fbff5e8b9cfd23b7984b44e
UENO Takahiro <uenotakahirooo@gmail.com>
2019-01-21 19:12:25 +0900

    comp 3-4

--- prog2/3-4.c
+++ prog2/3-4.c
@@ -13,11 +13,28 @@ long int g(int n){// n!! 再帰形
 long int f_gg(int n){ //n! ( n!! を適用 )
   if( n<2 ) return 1; return g(n)*g(n-1);}
 
+void show_results();
+
 int main(int argc, char *argv[]){
   if( argc==1 ) return 0;
   int n = atoi(argv[1]);
-  int l = atoi(argv[2]);
 
+  if(argc == 2){
+    show_results(n);
+    return 0;
+  }
+
+  if(argc == 3){
+    int m = atoi(argv[2]);
+    int i;
+    for(i=n;i<=m;i++){
+      show_results(i);
+    }
+  }
+  return 0;
+}
+
+void show_results(int n){
   printf("%d! = %ld ", n, f(n));
   if(f(n)==f_fn(n)){
     printf("f*n:. ");
@@ -29,6 +46,4 @@ int main(int argc, char *argv[]){
   } else {
     printf("%ld\n", f_gg(n));
   }
-
-  return 0;
 }
