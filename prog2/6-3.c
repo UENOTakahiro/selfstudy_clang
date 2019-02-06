580fdfda9da66d851bff392eac8c0b4ef1a33caf
UENO Takahiro <uenotakahirooo@gmail.com>
2019-02-06 12:51:45 +0900

    Fix 6-3 using STACK. got 100points

--- prog2/6-3.c
+++ prog2/6-3.c
@@ -37,18 +37,36 @@ int main(int argc, char *argv[]){
 
 void quick(int d[], int l, int r){
   int i, j, w, x;
-  if( l+1>=r ) return;
-  w= d[(l+r)/2]; i= l; j= r;
-  for(; i<j;){
-    while( d[i]<w ) i++;
-    while( d[j-1]>w ) j--;
-    if( i<j ){
-      x= d[i]; d[i]= d[j-1]; d[j-1]= x;
-      i++; j--;
-    }
+  STACKinit(50); STACKpush((STACKItem){l,r});
+
+  while(STACKcount()>0){
+    STACKItem p=STACKpop();
+      if( p.l+1>=p.r ) continue;
+      w= d[(p.l+p.r)/2]; i= p.l; j= p.r;
+      for(; i<j;){
+        while( d[i]<w ) i++;
+        while( d[j-1]>w ) j--;
+        if( i<j ){
+          x= d[i]; d[i]= d[j-1]; d[j-1]= x;
+          i++; j--;
+        }
+      }
+    STACKpush((STACKItem){i, p.r});
+    STACKpush((STACKItem){p.l, j});
   }
-  quick(d,i,r);
-  quick(d,l,j);
+
+  // if( l+1>=r ) return;
+  // w= d[(l+r)/2]; i= l; j= r;
+  // for(; i<j;){
+  //   while( d[i]<w ) i++;
+  //   while( d[j-1]>w ) j--;
+  //   if( i<j ){
+  //     x= d[i]; d[i]= d[j-1]; d[j-1]= x;
+  //     i++; j--;
+  //   }
+  // }
+  // quick(d,i,r);
+  // quick(d,l,j);
 }
 
 void generate(int n,int d[]){int i;srand(100*n+1);int
