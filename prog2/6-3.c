49e47954663a5e756d2cc125c77488d7d6b15f68
UENO Takahiro <uenotakahirooo@gmail.com>
2019-02-06 12:42:10 +0900

    Fix 6-3. got 100points.

--- prog2/6-3.c
+++ prog2/6-3.c
@@ -8,6 +8,9 @@ int count = 0;
 void generate(int n, int d[]);
    // d[0:n) に非負整数値を入れてくる
 
+void quick(int d[], int l, int r);
+// 配列 d[l:r) を昇順に整列する
+
 int main(int argc, char *argv[]){
 
   int n = atoi(argv[1]);
@@ -15,24 +18,37 @@ int main(int argc, char *argv[]){
   d = calloc(n, sizeof(int));
   generate(n, d);
 
-  // int i;
-  // for(i=0;i<n;i++){
-  //   printf("%d\n", d[i]);
-  // }
+  quick(d, 0, n);
 
-  STACKinit((n);
-  // int k;
-  // for(k=0;k<n;k++){
-  //   if(STACKpush(d[k])==NULL){
-  //     AMinsert(d[k]);
-  //   } else {
-  //     count++;
-  //   }
-  // }
+  int count;
+  int i;
+  for(i=0;i<n;i++){
+    if(d[i+1]==d[i]){
+      count++;
+    } else {
+      continue;
+    }
+  }
 
   printf("%d\n", count);
 
   return 0;
+}
+
+void quick(int d[], int l, int r){
+  int i, j, w, x;
+  if( l+1>=r ) return;
+  w= d[(l+r)/2]; i= l; j= r;
+  for(; i<j;){
+    while( d[i]<w ) i++;
+    while( d[j-1]>w ) j--;
+    if( i<j ){
+      x= d[i]; d[i]= d[j-1]; d[j-1]= x;
+      i++; j--;
+    }
+  }
+  quick(d,i,r);
+  quick(d,l,j);
 }
 
 void generate(int n,int d[]){int i;srand(100*n+1);int
